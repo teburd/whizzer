5212389dcabd2c1de685e410bc9a2132f8af4432
Tom Burdick <thomas.burdick@gmail.com>
2010-09-13 19:21:10 -0500

    basic client test stubs setup

--- tests/test_client.py
+++ tests/test_client.py
@@ -18,6 +18,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+
 import sys
 import time
 import unittest
@@ -27,19 +28,11 @@ sys.path.insert(0, '..')
 
 from whizzer.client import SocketClient
 
-class TestConnection(object):
+class TestTcpClient(unittest.TestCase):
+    """Functional test for TcpClient."""
     pass
 
-class TestSocketClient(object):
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-class TestTcpClient(object):
-    pass
-
-class TestUnixClient(object):
+class TestUnixClient(unittest.TestCase):
+    """Functional test for UnixClient."""
     pass
 
